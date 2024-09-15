b8d19a952ac0ec419cd90f9cccc1d59a9e81e9d0
mark.wang <wxgter@gmail.com>
2024-09-15 22:50:35 +0800

    add ks score and test

--- python/polars_credit/metrics.py
+++ python/polars_credit/metrics.py
@@ -1,21 +1,8 @@
 import polars as pl
 
 
-def roc_auc_score(col_true: str, col_pred: str):
-    """
-    Calculate the ROC AUC score using Polars expressions.
-
-    This function computes the Receiver Operating Characteristic Area Under the Curve
-    (ROC AUC) score for binary classification problems using Polars expressions.
-
-    Args:
-        col_true (str): Name of the column containing true binary labels.
-        col_pred (str): Name of the column containing predicted probabilities.
-
-    Returns
-    -------
-        pl.Expr: A Polars expression that computes the ROC AUC score.
-    """
+def roc_curve(col_true: str, col_pred: str):
+    """Calculate the ROC curve using Polars expressions."""
     sort_true = pl.col(col_true).sort_by(pl.col(col_pred), descending=True)
 
     cum_tp = sort_true.eq(1).cum_sum()
@@ -27,6 +14,19 @@ def roc_auc_score(col_true: str, col_pred: str):
     tpr = cum_tp / pos
     fpr = cum_fp / neg
 
+    return tpr, fpr
+
+
+def roc_auc_score(col_true: str, col_pred: str):
+    """Calculate the ROC AUC score using Polars expressions."""
+    tpr, fpr = roc_curve(col_true, col_pred)
     roc_auc = ((fpr - fpr.shift(1)) * (tpr + tpr.shift(1)) / 2).sum().alias("roc_auc")
 
     return roc_auc
+
+
+def ks_score(col_true: str, col_pred: str):
+    """Calculate the Kolmogorov-Smirnov (KS) score using Polars expressions."""
+    tpr, fpr = roc_curve(col_true, col_pred)
+    ks_score = (tpr - fpr).abs().max().alias("ks_score")
+    return ks_score
